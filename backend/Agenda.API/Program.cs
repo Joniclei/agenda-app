df5ed454a14196853d7c8b62fd9be67420109c19
joniclei bernardo <Admin@jonicleis-MacBook-Pro.local>
2026-04-12 20:57:26 -0300

    feat: adicionado o Cors

--- backend/Agenda.API/Program.cs
+++ backend/Agenda.API/Program.cs
@@ -9,10 +9,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
+
 
 if (app.Environment.IsDevelopment())
 {
